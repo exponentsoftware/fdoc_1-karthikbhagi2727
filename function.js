d45566181843d7f3f95602228d9815e57b12a7ab
karthikbhagi2727 <16j41a0211@gmail.com>
2021-03-03 17:56:52 +0530

    1a completed

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+/*this function is used to count which word is appeared more number of times*/
 let countWords = (str, word1, word2) => { 
     if(str.split(word1).length-1 > str.split(word2).length-1){
         return word1;
@@ -9,4 +10,4 @@ let countWords = (str, word1, word2) => {
 let paragraph =
   "This paragraph contains some unrelated data created just for the sake of counting the occurrences of the words in this paragraph so will add some random strings now for for created paragraph paragraph";
 
-console.log(countWords(paragraph, "for"));
+console.log(countWords(paragraph, "for","this"));
