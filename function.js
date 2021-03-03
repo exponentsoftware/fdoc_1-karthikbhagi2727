a6a0af3dcdd3f0d24e02e5eefe9ccfa05692531f
karthikbhagi2727 <16j41a0211@gmail.com>
2021-03-03 18:19:15 +0530

    1b completed

--- function.js
+++ function.js
@@ -11,3 +11,10 @@ let paragraph =
   "This paragraph contains some unrelated data created just for the sake of counting the occurrences of the words in this paragraph so will add some random strings now for for created paragraph paragraph";
 
 console.log(countWords(paragraph, "for","this"));
+
+/*this function is used to clear the text*/
+let  cleanText = (str) => str.replace(/[^a-zA-Z ]/g, "");
+
+let paragraph2 = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
+
+console.log(cleanText(paragraph2));
