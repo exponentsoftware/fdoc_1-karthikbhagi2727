ea3bac3964919b47cd8af8c22b4844080d933dcf
karthikbhagi2727 <16j41a0211@gmail.com>
2021-03-03 17:54:13 +0530

    1a completed

---
+++ function.js
@@ -0,0 +1,12 @@
+let countWords = (str, word1, word2) => { 
+    if(str.split(word1).length-1 > str.split(word2).length-1){
+        return word1;
+    }
+    else{
+        return word2;
+    }
+}
+let paragraph =
+  "This paragraph contains some unrelated data created just for the sake of counting the occurrences of the words in this paragraph so will add some random strings now for for created paragraph paragraph";
+
+console.log(countWords(paragraph, "for"));
